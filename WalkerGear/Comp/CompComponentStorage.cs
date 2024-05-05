0656d0d98ff37bd6125a3cf11269bb890be0289e
asvc33 <92707795+hhyzbd@users.noreply.github.com>
2024-05-05 16:54:56 +0800

    Add files via upload

---
+++ WalkerGear/Comp/CompComponentStorage.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace WalkerGear
+{
+    public class CompComponentStorage:ThingComp
+    {
+        public Building_Storage Parent => (Building_Storage)this.parent;
+        public bool needMaintenance;
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+            CheckMaintenance();
+        }
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            CheckMaintenance();
+        }
+        private void CheckMaintenance()
+        {
+            foreach (Thing thing in Parent.slotGroup.HeldThings)
+            {
+                if (thing is ThingWithComps twc && twc.TryGetComp<CompWalkerComponent>(out CompWalkerComponent c) && c.NeedMaintenance)
+                {
+                    needMaintenance = true;
+                    break;
+                }
+            }
+            needMaintenance = false;
+        }
+    }
+}

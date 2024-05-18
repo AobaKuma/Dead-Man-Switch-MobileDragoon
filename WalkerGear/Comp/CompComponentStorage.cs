3451f57d4e09435086b31cd9471b2cb1a12c3d1d
asvc33 <92707795+hhyzbd@users.noreply.github.com>
2024-05-18 18:43:38 +0800

    Add files via upload

---
+++ WalkerGear/Comp/CompComponentStorage.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using Verse;
+
+namespace WalkerGear
+{
+    public class CompComponentStorage:ThingComp
+    {
+        public Building_Storage Parent => (Building_Storage)this.parent;
+        public Thing maintanenceTar;        
+        public bool CheckMaintenance()
+        {
+            maintanenceTar=null;
+            foreach (Thing thing in Parent.slotGroup.HeldThings)
+            {
+                if (thing is ThingWithComps twc && twc.TryGetComp<CompWalkerComponent>(out CompWalkerComponent c) && c.NeedMaintenance)
+                {
+                    maintanenceTar = thing;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

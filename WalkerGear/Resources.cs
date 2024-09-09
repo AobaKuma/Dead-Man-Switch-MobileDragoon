c8fff23d12c7a0e70d63c65bf1e5c848ada47211
Aoba <sakilomo@gmail.com>
2024-09-09 17:16:34 +0800

    upd

--- WalkerGear/Resources.cs
+++ WalkerGear/Resources.cs
@@ -6,10 +6,19 @@ namespace WalkerGear
     [StaticConstructorOnStartup]
     public static class Resources
     {
+        //安全裝置的Icon
         public static readonly Texture2D GetSafetyIcon_Disabled = ContentFinder<Texture2D>.Get("UI/Safety_Disabled");
         public static readonly Texture2D GetSafetyIcon = ContentFinder<Texture2D>.Get("UI/Safety");
         public static readonly Texture2D GetOutIcon = ContentFinder<Texture2D>.Get("UI/GetOffWalker");
+
+        //裝配介面的Icon
         public static readonly Texture2D rotateButton = ContentFinder<Texture2D>.Get("UI/Rotate");
         public static readonly Texture2D rotateOppoButton = ContentFinder<Texture2D>.Get("UI/RotateOppo");
+
+        //彈射裝置的Icon
+        public static readonly Texture2D catapultLaunch = ContentFinder<Texture2D>.Get("UI/CatapultLaunch");
+        public static readonly Texture2D catapultThrow = ContentFinder<Texture2D>.Get("UI/CatapultThrow");
+        public static readonly Texture2D catapultEject = ContentFinder<Texture2D>.Get("UI/CatapultEject");
+
     }
 }
---
+++ WalkerGear/Stat/StatPart_MoveSpeed.cs
@@ -0,0 +1,64 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace WalkerGear
+{
+    public class StatPart_MoveSpeed : StatPart
+    {
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (IsWorking(req, out var def))
+            {
+                return "WG_ArmorSpeed".Translate() + " {0} c/s".Formatted(GetValue(req, def).ToString("0.##"));
+            }
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (IsWorking(req, out var def))
+            {
+                val = GetValue(req, def);
+            }
+        }
+        private float GetValue(StatRequest req, WalkerGear_Core def)
+        {
+            float baseStat = def.GetStatValue(StatDefOf.MoveSpeed);
+            IEnumerable<Apparel> enumerable = GetPawn(req).apparel?.WornApparel;
+            foreach (Apparel item in enumerable ?? Enumerable.Empty<Apparel>())
+            {
+                baseStat += item.def.equippedStatOffsets.GetStatOffsetFromList(StatDefOf.MoveSpeed);
+            }
+            return baseStat;
+        }
+        private Pawn GetPawn(StatRequest req)
+        {
+            if (req.Thing is Pawn p)
+            {
+                return p;
+            }
+            Log.Warning("Error: pawn not exist");
+            return null;
+        }
+        private bool IsWorking(StatRequest req, out WalkerGear_Core def)
+        {
+            def = null;
+            if (req.HasThing && req.Thing is Pawn pawn)
+            {
+                IEnumerable<Apparel> enumerable = pawn.apparel?.WornApparel;
+                foreach (Apparel item in enumerable ?? Enumerable.Empty<Apparel>())
+                {
+                    if (item is WalkerGear_Core walkerGear_Core)
+                    {
+                        def = walkerGear_Core;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
+
